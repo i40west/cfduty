a769dfcc1bfaf6fd3276e256872f42d3b4815cdb
Jeremy Nixon <16359642+i40west@users.noreply.github.com>
2024-10-31 17:11:31 -0400

    still send healthy alert if cf api calls fail

--- src/worker.js
+++ src/worker.js
@@ -9,89 +9,101 @@ const cf_api_url = 'https://api.cloudflare.com/client/v4';
 export class AlertWorkflow extends WorkflowEntrypoint {
     async run(event, step) {
 
-        const alert = await step.do('create alert', {
-            retries: {
-                limit: 20,
-                delay: 5,
-                backoff: 'linear',
-            },
-            timeout: '30 minutes',
-        }, async () => {
-                // treesitter insists on this indentation for some reason
-                const body = event.payload;
+        const alert = await step.do('create alert', { retries: { limit: 1 } }, async () => {
+            const body = event.payload;
 
-                const alert_name = body.data.name;
-                const service_name = alert_name.split('-')[0];
-                const service_component = alert_name.split('-')[1];
-                const key = this.env[`KEY_${service_name}`];
+            const alert_name = body.data.name;
+            const service_name = alert_name.split('-')[0];
+            const service_component = alert_name.split('-')[1];
+            const key = this.env[`KEY_${service_name}`];
 
-                // 'Healthy' notifications should not create a page, so set severity to info.
-                // If all health checks for this service are healthy, resolve the incident.
-                // If we don't have a CF API token, do nothing.
-                let severity = 'critical';
-                let action = 'trigger';
-                if (body.data.status === 'Healthy') {
-                    severity = 'info';
-                    console.info('got healthy notification for ' + alert_name);
+            let severity = 'critical';
+            let action = 'trigger';
 
-                    // Call the Cloudflare API to get the status of all health checks
-                    if (this.env.CF_API_TOKEN && this.env.CF_ZONE_ID) {
-                        const url = `${cf_api_url}/zones/${this.env.CF_ZONE_ID}/healthchecks`;
-                        const response = await fetch(url, {
-                            method: 'GET',
-                            headers: {
-                                'Authorization': 'Bearer ' + this.env.CF_API_TOKEN,
+            // 'Healthy' notifications should not create a page, so set severity to info.
+            // If all health checks for this service are healthy, resolve the incident.
+            // If we don't have a CF API token, do nothing.
+            if (body.data.status === 'Healthy') {
+                severity = 'info';
+                console.info('got healthy notification for ' + alert_name);
+
+                // Call the Cloudflare API to get the status of all health checks.
+                // This should be retried several times, but if it ultimately fails,
+                // we should continue with the workflow and send the alert anyway.
+                if (this.env.CF_API_TOKEN && this.env.CF_ZONE_ID) {
+                    let resolve = false;
+                    try {
+                        resolve = await step.do('call cloudflare api', {
+                            retries: {
+                                limit: 5,
+                                delay: 5,
+                                backoff: 'linear',
                             },
-                        });
+                            timeout: '5 minutes',
+                        }, async () => {
 
-                        const json = await response.json();
+                                const url = `${cf_api_url}/zones/${this.env.CF_ZONE_ID}/healthchecks`;
+                                const response = await fetch(url, {
+                                    method: 'GET',
+                                    headers: {
+                                        'Authorization': 'Bearer ' + this.env.CF_API_TOKEN,
+                                    },
+                                });
 
-                        if (json.success !== true) {
-                            console.error('Cloudflare API error:', json.errors[0].code, json.errors[0].message);
-                            throw new Error('Cloudflare API error: ' + json.errors[0].message);
-                        }
+                                const json = await response.json();
 
-                        let all_healthy = true;
-                        for (let i = 0; i < json.result.length; i++) {
-                            const result = json.result[i];
-                            // We are calling the API so quickly that sometimes it hasn't caught up
-                            // and still thinks the service is down, so skip the service we just
-                            // got a notification for.
-                            if (result.name === alert_name) {
-                                continue;
-                            }
-                            if (result.name.split('-')[0] === service_name) {
-                                if (result.status !== 'healthy') {
-                                    all_healthy = false;
-                                    console.info(result.name + ' is not healthy');
-                                    break;
+                                if (json.success !== true) {
+                                    console.error('Cloudflare API error:', json.errors[0].code, json.errors[0].message);
+                                    throw new Error('Cloudflare API error: ' + json.errors[0].message);
                                 }
-                            }
-                        }
 
-                        if (all_healthy) {
-                            console.log('all healthy, resolving incident');
-                            action = 'resolve';
-                        }
-                    } else {
-                        console.warn('no CF_API_TOKEN or CF_ZONE_ID, not checking other health checks');
+                                let all_healthy = true;
+                                for (let i = 0; i < json.result.length; i++) {
+                                    const result = json.result[i];
+                                    // We are calling the API so quickly that sometimes it hasn't caught up
+                                    // and still thinks the service is down, so skip the service we just
+                                    // got a notification for.
+                                    if (result.name === alert_name) {
+                                        continue;
+                                    }
+                                    if (result.name.split('-')[0] === service_name) {
+                                        if (result.status !== 'healthy') {
+                                            all_healthy = false;
+                                            console.info(result.name + ' is not healthy');
+                                            break;
+                                        }
+                                    }
+                                }
+                                return all_healthy;
+                            }); // end of step.do
+                    } catch (e) {
+                        console.error(e.message);
+                        resolve = false;
+                    }
+
+                    if (resolve) {
+                        console.log('all healthy, resolving incident');
+                        action = 'resolve';
                     }
                 } else {
-                    console.info('got unhealthy notification for ' + alert_name);
+                    console.warn('no CF_API_TOKEN or CF_ZONE_ID, not checking other health checks');
                 }
+            } else {
+                console.info('got unhealthy notification for ' + alert_name);
+            }
 
-                return {
-                    routing_key: key,
-                    event_action: action,
-                    dedup_key: service_name,
-                    payload: {
-                        summary: `${alert_name} is ${body.data.status}: ${body.data.reason}`,
-                        source: service_name,
-                        severity: severity,
-                        component: service_component,
-                    },
-                };
-            }); // end of step.do
+            return {
+                routing_key: key,
+                event_action: action,
+                dedup_key: service_name,
+                payload: {
+                    summary: `${alert_name} is ${body.data.status}: ${body.data.reason}`,
+                    source: service_name,
+                    severity: severity,
+                    component: service_component,
+                },
+            };
+        }); // end of step.do
 
         await step.do('send pagerduty alert', {
             retries: {
